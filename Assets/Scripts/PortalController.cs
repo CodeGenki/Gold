7c583c8fce1b3603d189a8c9fa39d846366a75e6
CodeGenki <genoji37@gmail.com>
2017-07-07 16:16:51 -0500

    Added a basic portal script and removed unused scripts

---
+++ Assets/Scripts/PortalController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalController : MonoBehaviour {
+
+    public int portalID;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name == "Player")
+        {
+            // Change Scene to specific portal id
+            SceneManager.LoadScene(portalID);
+        }
+    }
+}
